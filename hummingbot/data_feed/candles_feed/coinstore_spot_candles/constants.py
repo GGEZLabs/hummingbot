d57922ddb8b095c36097eb38d12011d7fc859bac
shaker113 <shaker.baker113@gmail.com>
2025-01-12 22:26:02 +0300

    Refactor backtesting components: update default interval, streamline data handling, and remove unused imports

--- hummingbot/data_feed/candles_feed/coinstore_spot_candles/constants.py
+++ hummingbot/data_feed/candles_feed/coinstore_spot_candles/constants.py
@@ -8,29 +8,22 @@ CANDLES_ENDPOINT = "/v1/market/kline"
 
 WSS_URL = "wss://ws.coinstore.com/s/ws"
 
-
 INTERVALS = bidict(
     {
-        "1s": "1s",
         "1m": "1m",
-        "3m": "3m",
         "5m": "5m",
         "15m": "15m",
         "30m": "30m",
         "1h": "1h",
-        "2h": "2h",
         "4h": "4h",
-        "6h": "6h",
-        "8h": "8h",
         "12h": "12h",
         "1d": "1d",
-        "3d": "3d",
         "1w": "1w",
         "1M": "1M",
     }
 )
 
-DEFAULT_INTERVAL = "1m"
+DEFAULT_INTERVAL = "1h"
 WS_INTERVALS = {
     "1m": "min_1",
     "5m": "min_5",
