4d8f44ef48640f2cd45e2408b18a02eb0d4aa04a
shaker113 <shaker.baker113@gmail.com>
2025-04-06 16:16:38 +0300

    Add script to copy configuration files for multiple hummingbot instances

---
+++ copy_conf_files.sh
@@ -0,0 +1,19 @@
+sudo cp -r conf/connectors/* conf/hummingbot-1/connectors/
+sudo cp -r conf/connectors/* conf/hummingbot-2/connectors/
+sudo cp -r conf/connectors/* conf/hummingbot-3/connectors/
+
+sudo cp -r conf/strategies/* conf/hummingbot-1/strategies/
+sudo cp -r conf/strategies/* conf/hummingbot-2/strategies/
+sudo cp -r conf/strategies/* conf/hummingbot-3/strategies/
+ 
+sudo cp -r conf/scripts/* conf/hummingbot-1/scripts/
+sudo cp -r conf/scripts/* conf/hummingbot-2/scripts/
+sudo cp -r conf/scripts/* conf/hummingbot-3/scripts/
+ 
+sudo cp -r conf/controllers/* conf/hummingbot-1/controllers/
+sudo cp -r conf/controllers/* conf/hummingbot-2/controllers/
+sudo cp -r conf/controllers/* conf/hummingbot-3/controllers/
+ 
+sudo cp -r conf/.password_verification conf/hummingbot-1/
+sudo cp -r conf/.password_verification conf/hummingbot-2/
+sudo cp -r conf/.password_verification conf/hummingbot-3/
