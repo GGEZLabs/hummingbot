c05daf5e32419910f2f3786508d6c2bbfede9326
shaker113 <shaker.baker113@gmail.com>
2025-04-16 15:00:16 +0300

    Refactor copy_conf_files.sh to accept instance name as an argument and streamline configuration file copying

--- copy_conf_files.sh
+++ copy_conf_files.sh
@@ -1,19 +1,19 @@
-sudo cp -r conf/connectors/* conf/hummingbot-1/connectors/
-sudo cp -r conf/connectors/* conf/hummingbot-2/connectors/
-sudo cp -r conf/connectors/* conf/hummingbot-3/connectors/
+if [ -z "$1" ]; then
+  echo "Error: No instance name provided." >&2 # Print error to standard error
+  echo "Usage: sudo $0 <instance_name>" >&2
+  exit 1 # Exit with a non-zero status to indicate failure
+fi
 
-sudo cp -r conf/strategies/* conf/hummingbot-1/strategies/
-sudo cp -r conf/strategies/* conf/hummingbot-2/strategies/
-sudo cp -r conf/strategies/* conf/hummingbot-3/strategies/
- 
-sudo cp -r conf/scripts/* conf/hummingbot-1/scripts/
-sudo cp -r conf/scripts/* conf/hummingbot-2/scripts/
-sudo cp -r conf/scripts/* conf/hummingbot-3/scripts/
- 
-sudo cp -r conf/controllers/* conf/hummingbot-1/controllers/
-sudo cp -r conf/controllers/* conf/hummingbot-2/controllers/
-sudo cp -r conf/controllers/* conf/hummingbot-3/controllers/
- 
-sudo cp -r conf/.password_verification conf/hummingbot-1/
-sudo cp -r conf/.password_verification conf/hummingbot-2/
-sudo cp -r conf/.password_verification conf/hummingbot-3/
+INSTANCE_NAME="$1"
+
+sudo mkdir -p conf/$INSTANCE_NAME/connectors/
+sudo cp -r conf/connectors/* "conf/$INSTANCE_NAME/connectors/"
+sudo mkdir -p conf/$INSTANCE_NAME/strategies/
+sudo cp -r conf/strategies/* "conf/$INSTANCE_NAME/strategies/"
+sudo mkdir -p conf/$INSTANCE_NAME/scripts/
+sudo cp -r conf/scripts/* "conf/$INSTANCE_NAME/scripts/"
+sudo mkdir -p conf/$INSTANCE_NAME/controllers/
+sudo cp -r conf/controllers/* "conf/$INSTANCE_NAME/controllers/"
+sudo cp -r conf/.password_verification "conf/$INSTANCE_NAME/"
+
+echo "Configuration files copied successfully to conf/$INSTANCE_NAME/"
