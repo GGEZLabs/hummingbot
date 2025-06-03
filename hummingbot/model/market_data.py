e34955732a714b5c771d020968b36771ad15e0f1
shaker113 <shaker.baker113@gmail.com>
2025-06-03 16:23:18 +0300

    add balance column to MarketData table

--- hummingbot/model/market_data.py
+++ hummingbot/model/market_data.py
@@ -8,9 +8,7 @@ from hummingbot.model.decimal_type_decorator import SqliteDecimal
 
 class MarketData(HummingbotBase):
     __tablename__ = "MarketData"
-    __table_args__ = (
-        Index("timestamp", "exchange", "trading_pair"),
-    )
+    __table_args__ = (Index("timestamp", "exchange", "trading_pair"),)
 
     timestamp = Column(SqliteDecimal(6), primary_key=True, nullable=False)
     exchange = Column(Text, nullable=False)
@@ -19,7 +17,8 @@ class MarketData(HummingbotBase):
     best_bid = Column(SqliteDecimal(6), nullable=False)
     best_ask = Column(SqliteDecimal(6), nullable=False)
     order_book = Column(JSON)
+    balance = Column(JSON)
 
     def __repr__(self) -> str:
         list_of_fields = [f"{name}: {value}" for name, value in inspect.getmembers(self) if isinstance(value, Column)]
-        return ','.join(list_of_fields)
+        return ",".join(list_of_fields)
