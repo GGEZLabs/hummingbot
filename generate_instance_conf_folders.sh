90d0720bd611ca551d15bf54d77080ee5e15dbc2
shaker113 <shaker.baker113@gmail.com>
2025-08-18 21:38:42 +0300

    Refactor copy_conf_files.sh to streamline directory creation and remove redundant copy commands

--- generate_instance_conf_folders.sh
+++ generate_instance_conf_folders.sh
@@ -7,13 +7,11 @@ fi
 INSTANCE_NAME="$1"
 
 sudo mkdir -p conf/$INSTANCE_NAME/connectors/
-sudo cp -r conf/connectors/* "conf/$INSTANCE_NAME/connectors/"
 sudo mkdir -p conf/$INSTANCE_NAME/strategies/
-sudo cp -r conf/strategies/* "conf/$INSTANCE_NAME/strategies/"
 sudo mkdir -p conf/$INSTANCE_NAME/scripts/
-sudo cp -r conf/scripts/* "conf/$INSTANCE_NAME/scripts/"
 sudo mkdir -p conf/$INSTANCE_NAME/controllers/
-sudo cp -r conf/controllers/* "conf/$INSTANCE_NAME/controllers/"
 sudo cp -r conf/.password_verification "conf/$INSTANCE_NAME/"
+
+sudo mkdir -p logs/$INSTANCE_NAME/
 
 echo "Configuration files copied successfully to conf/$INSTANCE_NAME/"
